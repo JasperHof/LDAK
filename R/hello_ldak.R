fbf5fa938b9f326b8b30e9da851a1934355d6976
Jasper Pieter Hof <jasper.hof@qgg.au.dk>
2025-10-13 13:18:37 +0200

    first commit

---
+++ R/hello_ldak.R
@@ -0,0 +1,8 @@
+#' Say Hello
+#'
+#' A simple function that prints "Hello, world!"
+#' @return NULL
+#' @export
+hello <- function() {
+  print("Welcome to LDAK!")
+}
---
+++ R/ldak.R
@@ -0,0 +1,39 @@
+#' Run LDAK executable
+#'
+#' This function automatically detects the operating system and runs
+#' the corresponding LDAK binary shipped with the package.
+#'
+#' @param args Character vector of arguments to pass to LDAK
+#' @return The output of the system call
+#' @export
+run_ldak <- function(args = character()) {
+  os <- Sys.info()[["sysname"]]
+
+  exe_name <- switch(
+    os,
+    "Linux"   = "ldak6.1.linux",
+    "Darwin"  = "ldak6.1.mac",
+    "Windows" = "ldak6.1.exe",
+    stop("Unsupported OS: ", os)
+  )
+
+  exe_path <- system.file("bin", exe_name, package = "LDAK")
+
+  if (exe_path == "") {
+    stop("Could not find the LDAK binary for your OS.")
+  }
+
+  # Ensure executable permissions on Unix-like systems
+  if (os != "Windows") {
+    Sys.chmod(exe_path, mode = "755")
+  }
+
+  # Run the executable
+  exit_code <- system2(exe_path, args = args, stdout = "", stderr = "")
+
+  if (exit_code != 0) {
+    stop("LDAK execution failed with exit code ", exit_code)
+  }
+
+  invisible(exit_code)
+}
